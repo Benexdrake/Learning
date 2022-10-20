caac3d76f27cfd7913d4ceefa1a47197c9ddfee6
BeneX Drake <50136177+Benexdrake@users.noreply.github.com>
2022-10-20 17:33:46 +0200

    Sortierte Ordnerstruktur gefunden in dem Test

--- Console Spielerei/Program.cs
+++ Console Spielerei/Program.cs
@@ -1,55 +1,126 @@
 using Console_Spielerei;
+using System.IO;
 
-List<Group> groups = new List<Group>();
+Group g = new Group();
+
+List<string> list = new List<string>();
 
 // Start Directory
-FindDirectories("D:\\Eigene Projekte");
+//FindDirectories("D:\\Eigene Projekte");
 
-string FindDirectories(string path)
+string root = "C:\\Users\\Benex\\Desktop\\Projects";
+
+var directories = Directory.GetDirectories(root,"",SearchOption.TopDirectoryOnly);
+
+Print(directories);
+
+
+
+void Print(string[] paths)
 {
-    var directories = Directory.GetDirectories(path);
-    
-    foreach (var directory in directories)
+    Group g = new Group();
+    if (paths != null)
     {
-        List<Group> groupsChilds = new List<Group>();
-        var directoryName = Path.GetDirectoryName(directory);
-        if (!string.IsNullOrWhiteSpace(directory))
+        foreach (var path in paths)
         {
-            Group group = new Group();
-            FindDirectories(directory);
-
-            var childs = Directory.GetDirectories(directory);
-            foreach (var child in childs)
-            {
-                groupsChilds.Add(new Group()
-                {
-                    TotalPath = child,
-                    Name = new DirectoryInfo(child).Name
-            });
-            }
-            group.Children = groupsChilds;
-            group.TotalPath = directory;
-            group.Name = new DirectoryInfo(directory).Name;
-            groups.Add(group);
+            list.Add(path);
+            Console.WriteLine(path);
+            var ps = GetDirectories(path);
+            Print(ps);
         }
     }
-    return "";
 }
 
 
-
-
-foreach (var item in groups)
+string[] GetDirectories(string path)
 {
-    Console.WriteLine(">>Total Path: {0}",item.TotalPath);
-    Console.WriteLine();
-    
-    foreach (var childs in item.Children)
-    {
-        Console.WriteLine(">>>>> Child of {0}: {1} ",item.Name, childs.Name);
-    }
-    Console.WriteLine();
+    return Directory.GetDirectories(path, "", SearchOption.TopDirectoryOnly);
 }
+
+
+Group FindGroup(string path)
+{
+    var group = new Group();
+
+
+    return group;
+}
+
+
+//Group FindDirectory(string path)
+//{
+//    var group = new Group();
+//
+//    List<Group> groups = new List<Group>();
+//
+//    var name = new DirectoryInfo(path).Name;
+//
+//    var folder = path.Substring(0, (path.Length - name.Length));
+//
+//    var directories = Directory.GetDirectories(folder);
+//
+//    foreach (var d in directories)
+//    {
+//        groups.Add(new Group()
+//        {
+//            Name = new DirectoryInfo(d).Name,
+//            TotalPath = d
+//        });
+//    }
+//
+//    group.Children = groups;
+//    group.Name = name;
+//    group.TotalPath = path;
+//
+//    return group;
+//}
+
+Console.WriteLine();
+//string FindDirectories(string path)
+//{
+//    var directories = Directory.GetDirectories(path);
+//    
+//    foreach (var directory in directories)
+//    {
+//        List<Group> groupsChilds = new List<Group>();
+//        var directoryName = Path.GetDirectoryName(directory);
+//        if (!string.IsNullOrWhiteSpace(directory))
+//        {
+//            Group group = new Group();
+//            FindDirectories(directory);
+//
+//            var childs = Directory.GetDirectories(directory);
+//            foreach (var child in childs)
+//            {
+//                groupsChilds.Add(new Group()
+//                {
+//                    TotalPath = child,
+//                    Name = new DirectoryInfo(child).Name
+//            });
+//            }
+//            group.Children = groupsChilds;
+//            group.TotalPath = directory;
+//            group.Name = new DirectoryInfo(directory).Name;
+//            groups.Add(group);
+//        }
+//    }
+//    return "";
+//}
+
+
+
+
+//foreach (var item in groups)
+//{
+//    Console.WriteLine(">>Total Path: {0}",item.TotalPath);
+//    Console.WriteLine();
+//    
+//    foreach (var childs in item.Children)
+//    {
+//        Console.WriteLine(">>>>> Child of {0}: {1} ",item.Name, childs.Name);
+//    }
+//    Console.WriteLine();
+//}
 
 //string downloadFolder = "F:\\BenR\\LogicTesting\\Download\\";
 //
